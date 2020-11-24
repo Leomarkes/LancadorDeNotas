56f3b02526376d474aa53f1c68ff38f15218c6f5
Hiago W Petris <hiago.petris@gmail.com>
2020-11-24 20:39:23 -0300

    Ajustes

--- app/src/main/java/com/example/lancadordenotas/MainActivity.java
+++ app/src/main/java/com/example/lancadordenotas/MainActivity.java
@@ -22,32 +22,37 @@ public class MainActivity extends AppCompatActivity {
         EditText nota2 = findViewById(R.id.editTextNota2);
         EditText freq = findViewById(R.id.editTextFreq);
 
-        Double n1 = Double.parseDouble(nota1.getText().toString());
-        Double n2 = Double.parseDouble(nota2.getText().toString());
-        int fq = Integer.parseInt(freq.getText().toString());
-        String n = name.getText().toString();
-
-        if (n.length() == 0){
+        if (name.length() == 0){
             Toast.makeText(this, "Insira um nome", Toast.LENGTH_SHORT).show();
-        } else if(n1 > 10 || n2 > 10){
-            Toast.makeText(this, "Somente notas entre 0 e 10", Toast.LENGTH_SHORT).show();
-        } else if(fq > 100){
-            Toast.makeText(this, "Frequência deve estar 0 e 100", Toast.LENGTH_SHORT).show();
-        }else {
+        } else if(nota1.length() == 0 || nota2.length() == 0 || freq.length() == 0){
+            Toast.makeText(this, "Insira todas as notas e a frequência", Toast.LENGTH_SHORT).show();
+        } else {
 
-            Intent it = new Intent(this, Situacao.class);
+            Double n1 = Double.parseDouble(nota1.getText().toString());
+            Double n2 = Double.parseDouble(nota2.getText().toString());
+            int fq = Integer.parseInt(freq.getText().toString());
+            String n = name.getText().toString();
 
-            Bundle params = new Bundle();
-            params.putString("name", n);
-            params.putDouble("nota1", n1);
-            params.putDouble("nota2", n2);
-            params.putInt("freq", fq);
+            if (n1 > 10 || n2 > 10) {
+                Toast.makeText(this, "Somente notas entre 0 e 10", Toast.LENGTH_SHORT).show();
+            } else if (fq > 100) {
+                Toast.makeText(this, "Frequência deve estar 0 e 100", Toast.LENGTH_SHORT).show();
+            } else {
 
-            it.putExtras(params);
+                Intent it = new Intent(this, Situacao.class);
 
-            startActivity(it);
+                Bundle params = new Bundle();
+                params.putString("name", n);
+                params.putDouble("nota1", n1);
+                params.putDouble("nota2", n2);
+                params.putInt("freq", fq);
 
-            finish();
+                it.putExtras(params);
+
+                startActivity(it);
+
+//            finish();
+            }
         }
     }
 }
